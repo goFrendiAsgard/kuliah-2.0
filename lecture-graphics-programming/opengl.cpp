528e16614b452bc59dfe506976591a6b0fa88bfd
goFrendiAsgard <gofrendiasgard@gmail.com>
2018-04-15 15:11:20 +0700

    adjust source code

--- lecture-graphics-programming/opengl.cpp
+++ lecture-graphics-programming/opengl.cpp
@@ -5,30 +5,30 @@ float angle = 0;
 
 void renderFunction()
 {
-    glClearColor(0.0, 0.0, 0.0, 0.0);
-    glClear(GL_COLOR_BUFFER_BIT);
-    angle += 0.0001;
-    glRotatef(angle, 0, 0, 1);
-    glColor3f(1.0, 1.0, 1.0);
-    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
-    glBegin(GL_POLYGON);
-        glVertex2f(-0.5, -0.5);
-        glVertex2f(-0.5, 0.5);
-        glVertex2f(0.5, 0.5);
-        glVertex2f(0.5, -0.5);
-    glEnd();
-    glFlush();
-    glutPostRedisplay();
+  glClearColor(0.0, 0.0, 0.0, 0.0);
+  glClear(GL_COLOR_BUFFER_BIT);
+  angle += 0.0001;
+  glRotatef(angle, 0, 0, 1);
+  glColor3f(1.0, 1.0, 1.0);
+  glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+  glBegin(GL_POLYGON);
+  glVertex2f(-0.5, -0.5);
+  glVertex2f(-0.5, 0.5);
+  glVertex2f(0.5, 0.5);
+  glVertex2f(0.5, -0.5);
+  glEnd();
+  glFlush();
+  glutPostRedisplay();
 }
 
 int main(int argc, char** argv)
 {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE);
-    glutInitWindowSize(500, 500);
-    glutInitWindowPosition(100, 100);
-    glutCreateWindow("OpenGL - First window demo");
-    glutDisplayFunc(renderFunction);
-    glutMainLoop();    
-    return 0;
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_SINGLE);
+  glutInitWindowSize(500, 500);
+  glutInitWindowPosition(100, 100);
+  glutCreateWindow("OpenGL - First window demo");
+  glutDisplayFunc(renderFunction);
+  glutMainLoop();    
+  return 0;
 }
