9491e4d20d5eb269429c11ce32bf1300e9511c5f
goFrendiAsgard <gofrendiasgard@gmail.com>
2018-09-04 09:38:43 +0700

    first meeting PD II

---
+++ lecture-basic-programming-2/FirstMeeting.java
@@ -0,0 +1,31 @@
+/*
+ * Nama : Go Frendi Gunawan
+ * No HP/Whatsappp : 089680868343
+ * Telegram : @gofrendi
+ * Materi : https://github.com/goFrendiAsgard/kuliah-2.0
+ */
+import java.util.ArrayList;
+public class FirstMeeting {
+    public static void main(String[] args) {
+        int a = 5; // a = 5
+        int b = a; // a = 5, b = 5
+        a = 7; // a = 7, b = 5
+        System.out.println(a); // 7
+        System.out.println(b); // 5
+
+        int[] c = {5, 6, 7}; // c = {5,6,7}
+        int[] d = c; // d = c = {5,6,7}
+        c[0] = 8; // d = c = {8,6,7}
+        System.out.println(c[0]); // 8
+        System.out.println(d[0]); // 8
+
+        int[] e = new int[2]; // e = {0,0}
+        e[0] = 1; // e = {1,0}
+        e[1] = 5; // e = {1,5}
+       
+        ArrayList f = new ArrayList();
+        f.add(1);
+        f.add(2);
+        System.out.println(f);
+    }
+}
