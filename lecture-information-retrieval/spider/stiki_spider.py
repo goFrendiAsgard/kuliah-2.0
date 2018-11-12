f24b6c0af7809cbedb893ddbb340af64674d9483
goFrendiAsgard <gofrendiasgard@gmail.com>
2018-11-12 10:28:21 +0700

    add scraping example

---
+++ lecture-information-retrieval/spider/stiki_spider.py
@@ -0,0 +1,19 @@
+import scrapy, io
+
+class StikiSpider(scrapy.Spider):
+    name = 'blogspider'
+    start_urls = ['http://stiki.ac.id']
+
+    def parse(self, response):
+        for title in response.css('.entry-title'):
+            judul = title.css('a ::text').extract_first()
+            if judul:
+                judul = judul.encode('utf-8')
+                f = open('output.txt', 'a')
+                f.write(judul)
+                f.close()
+            # yield {'title': judul}
+            
+
+        for next_page in response.css('a.pagination-next'):
+            yield response.follow(next_page, self.parse)
